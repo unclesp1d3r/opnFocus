1596af6782e987bc82459f8cfb41eea0e80fb063
UncleSp1d3r <unclespider@protonmail.com>
2024-02-15 14:53:00 -0500

    Basic boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # opnFocus
-Generate meaninigful output from your opnSense configuration file, inspired by TKCERT/pfFocus
+
+## Introduction
+
+`opnFocus` is a powerful tool developed in Golang, designed to generate meaningful output from your OPNsense configuration file. Inspired by the well-regarded `TKCERT/pfFocus`, opnFocus aims to provide a similar utility for the OPNsense community, enriching the experience and management of OPNsense configurations. This project is licensed under GPL-3.0.
+
+## Features
+
+- **Configuration Analysis:** Parse and analyze your OPNsense configuration files to extract vital information.
+- **Insightful Summaries:** Get summaries of your firewall rules, NAT port forwards, aliases, and more.
+- **Security Audits:** Perform basic security checks to identify potential vulnerabilities or misconfigurations.
+- **Export Options:** Export your analyzed data into various formats for further analysis or documentation purposes.
+
+## Installation
+
+To install opnFocus, ensure you have Golang installed on your system. Follow these steps:
+
+```bash
+# Clone the repository
+git clone https://github.com/unclesp1d3r/opnFocus.git
+
+# Navigate to the project directory
+cd opnFocus
+
+# Build the project
+go build
+```
+
+# Alternatively, you can install it directly
+
+```bash
+go install
+```
+
+## Usage
+
+To use opnFocus, run the following command:
+
+```bash
+./opnFocus <path-to-your-OPNsense-configuration-file>
+```
+
+For more detailed usage instructions, including the list of available options, run:
+
+```bash
+./opnFocus -h
+```
+
+## Contributing
+
+We welcome contributions! If you would like to help make opnFocus better, please follow our contributing guidelines:
+
+1. Fork the repository.
+2. Create a new branch for your feature or fix.
+3. Commit your changes.
+4. Push to the branch.
+5. Submit a pull request.
+   Please ensure your code adheres to the project's coding standards and include tests, if applicable.
+
+## License
+
+opnFocus is licensed under the GNU General Public License v3.0. For more information, please see the LICENSE file.
+
+## Support
+
+If you need help or have any questions, please open an issue in the GitHub issue tracker for this project.
+
+## Acknowledgements
+
+A special thanks to TKCERT/pfFocus for the inspiration behind opnFocus. Check out their project [here](https://github.com/TKCERT/pfFocus).
---
+++ cmd/root.go
@@ -0,0 +1,110 @@
+/*
+Copyright © 2024 UncleSp1d3r <unclespider@protonmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"encoding/xml"
+	"fmt"
+	goutbra "github.com/drewstinnett/gout-cobra"
+	"github.com/drewstinnett/gout/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "opnFocus",
+	Short: "Generate meaningful output from your opnSense configuration files.",
+	Long: `opnFocus is a tool to generate meaningful output from your opnSense configuration files. 
+It is designed to be used in a pipeline with other tools to generate reports, 
+or to be used as a standalone tool to generate human-readable output from your opnSense configuration files.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		dat, err := os.Open(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		dec := xml.NewDecoder(dat)
+		var doc Opnsense
+		if err := dec.Decode(&doc); err != nil {
+			log.Fatal(err)
+		}
+		gout.MustPrint(doc)
+	},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		err := goutbra.Cmd(cmd)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+	Args: cobra.ExactArgs(1),
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.opnFocus.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	err := goutbra.Bind(rootCmd, goutbra.WithHelp("Some help"))
+	if err != nil {
+		return
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".opnFocus" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".opnFocus")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
